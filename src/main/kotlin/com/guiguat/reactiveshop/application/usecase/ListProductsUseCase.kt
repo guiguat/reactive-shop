4f5e6a6fc1c6cf2226f7b55dc431b0be8b5e2520
guiguat <guaturadzn@gmail.com>
2020-12-29 17:37:52 -0300

    create list products use-case

--- src/main/kotlin/com/guiguat/reactiveshop/application/usecase/ListProductsUseCase.kt
+++ src/main/kotlin/com/guiguat/reactiveshop/application/usecase/ListProductsUseCase.kt
@@ -1,7 +1,13 @@
 package com.guiguat.reactiveshop.application.usecase
 
+import com.guiguat.reactiveshop.application.repositories.ProductRepository
+import com.guiguat.reactiveshop.domain.Product
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class ListProductsUseCase {
+class ListProductsUseCase @Autowired constructor(private val repo: ProductRepository) {
+    fun execute(): List<Product>{
+        return repo.findAll()
+    }
 }
