7879d4d843f8bc42ca2eced2e88adaa5bcae18b5
guiguat <guaturadzn@gmail.com>
2020-12-31 23:09:12 -0300

    create Order response DTO

--- src/main/kotlin/com/guiguat/reactiveshop/domain/OrderDTO.kt
+++ src/main/kotlin/com/guiguat/reactiveshop/domain/OrderDTO.kt
@@ -3,6 +3,7 @@ package com.guiguat.reactiveshop.domain
 import java.math.BigDecimal
 import java.util.*
 
-data class OrderDTO(val total: BigDecimal, val products: String){
-    fun toOrder() = Order(null, total, products, Date())
+data class OrderReqDTO(val total: BigDecimal, val products: String){
+    fun asOrder() = Order(null, total, products, Date())
 }
+class OrderResDTO(val id: String, val total: BigDecimal, val products: String, val date: Date, val status: OrderStatusCode)
