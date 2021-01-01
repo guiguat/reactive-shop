f09980f79baf77e52bd67a32e5f9e6103d5c0fab
guiguat <guaturadzn@gmail.com>
2021-01-01 03:11:52 -0300

    create status stream

--- src/main/kotlin/com/guiguat/reactiveshop/controller/OrderController.kt
+++ src/main/kotlin/com/guiguat/reactiveshop/controller/OrderController.kt
@@ -3,14 +3,15 @@ package com.guiguat.reactiveshop.controller
 import com.guiguat.reactiveshop.application.service.OrderService
 import com.guiguat.reactiveshop.application.service.OrderStatusService
 import com.guiguat.reactiveshop.domain.OrderReqDTO
-import com.guiguat.reactiveshop.domain.OrderResDTO
 import com.guiguat.reactiveshop.domain.OrderStatusCode
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.*
-import reactor.core.publisher.Mono
+import reactor.core.publisher.Flux
+import java.time.Duration
 
 @RestController()
 @RequestMapping("/order")
@@ -19,20 +20,18 @@ class OrderController @Autowired constructor(
     private val statusService: OrderStatusService){
 
     @PostMapping @ResponseStatus(HttpStatus.CREATED)
-    fun createOrder(@RequestBody order: OrderReqDTO): Mono<OrderResDTO> = service.create(order)
+    fun createOrder(@RequestBody order: OrderReqDTO) = service.create(order)
 
-    @GetMapping("{id}")
-    fun findById(@PathVariable id: String): Mono<ResponseEntity<OrderResDTO>> = service.findById(id).map { ok(it) }
+    @GetMapping("{id}") fun findById(@PathVariable id: String) = service.findById(id).map { ok(it) }
         .defaultIfEmpty(ResponseEntity.notFound().build())
 
     @PutMapping("update/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    fun updateStatus(
+    @ResponseStatus(HttpStatus.OK) fun updateStatus(
         @PathVariable id: String,
         @RequestParam("status") status: OrderStatusCode) = statusService.update(id, status).map { ok(it) }
             .defaultIfEmpty(ResponseEntity.notFound().build())
 
-    @PutMapping("stream/{id}")
-    fun cancelOrder(
-        @PathVariable id: String): Nothing = TODO("Not implemented yet")
+    @GetMapping("stream/{id}", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun streamStatus(@PathVariable id: String) = Flux
+        .interval(Duration.ofSeconds(5)).map{statusService.findById(id)}
 }
