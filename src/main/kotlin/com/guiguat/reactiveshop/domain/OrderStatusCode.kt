397afe06666a8caa4b96e4b282933b419eaa7f5c
guiguat <guaturadzn@gmail.com>
2020-12-31 23:10:59 -0300

    remove int values

--- src/main/kotlin/com/guiguat/reactiveshop/domain/OrderStatusCode.kt
+++ src/main/kotlin/com/guiguat/reactiveshop/domain/OrderStatusCode.kt
@@ -1,5 +1,5 @@
 package com.guiguat.reactiveshop.domain
 
-enum class OrderStatusCode (value: Int){
-    CANCELED(-1), CONFIRMATION(0), PREPARING(1), DELIVERING(2), COMPLETE(3),
+enum class OrderStatusCode{
+    CANCELED, CONFIRMATION, PREPARING, DELIVERING, COMPLETE
 }
